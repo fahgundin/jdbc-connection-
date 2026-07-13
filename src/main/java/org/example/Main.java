e317ba8bc488235a0f5f3f02b7139d98f3972a19
fahgundin <sfagundes.integra@gmail.com>
2026-07-13 15:15:44 -0400

    primeiro commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            int opcao = sc.nextInt();
+            if (opcao == 0){
+                break;
+            }
+            switch (opcao){
+                case(1):
+                    break;
+                case(2):
+                    break;
+                case(3):
+                    break;
+                case(4):
+                    break;
+                case(5):
+                    break;
+                case(6):
+                    break;
+            }
+
+
+        }
+
+    }
+}
